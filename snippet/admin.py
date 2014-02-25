91192b64c79c970b859e0f8e947d7ebbfdcfa582
David THENON <sveetch@gmail.com>
2014-02-25 04:22:13 +0100

    Add search form in model admin, bumping version

--- snippet/admin.py
+++ snippet/admin.py
@@ -5,6 +5,8 @@ from django.db.models import TextField
 from djangocodemirror.widgets import CodeMirrorWidget
 
 class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ['name']
     formfield_overrides = {
         TextField: {'widget': CodeMirrorWidget(config_name='cms_snippet')},
     }
